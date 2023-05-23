bea5e11514e73f3e55ed7716bf508e0944b552ef
Megan <128613914+UntouchableMegan@users.noreply.github.com>
2023-05-23 09:04:57 -0600

    Operacion con numeros

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 console.log("Hello World! ");
-alert("Bienvenido a Javascript")
+alert("Bienvenido a Javascript");
+/*
+Comentario multilinea
+*/
+let nombre = prompt("Cual es tu nombre?");
+console.log(nombre)
+console.log(typeof (nombre) )
+
+let numeroUno = prompt("Ingresa un numero");
+let numeroDos = prompt("Ingresa otro numero");
+let resultadoSuma = parseInt(numeroUno) + parseInt (numeroDos);
+
+let resultado = parseInt(numeroUno) + parseInt (numeroDos);
+
+console.log(`El resultado de la suma es: ${resultadoSuma} `); 
+
