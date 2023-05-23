03a3a241bbc72b9fb2a919e840cde29c90c195e7
Megan <128613914+UntouchableMegan@users.noreply.github.com>
2023-05-23 09:09:36 -0600

    Operaciones con numeros completa

--- main.js
+++ main.js
@@ -15,3 +15,11 @@ let resultado = parseInt(numeroUno) + parseInt (numeroDos);
 
 console.log(`El resultado de la suma es: ${resultadoSuma} `); 
 
+let resultadoSuma=parseInt(numerouno)+ parseInt(numerodos);
+        let resultadoResta=parseInt(numerouno)- parseInt(numerodos);
+        let resultadoMulti= parseInt(numerouno)*parseInt(numerodos);
+        let resultadodiv= parseInt(numerouno)/parseInt(numerodos);
+        console.log(`el resultado de la suma es: ${resultadoSuma}`);
+        console.log(`el resultado de la resta es: ${resultadoResta}`);
+        console.log(`el resultado de la multiplicacion es: ${resultadoMulti}`);
+        console.log(`el resultado de la division es: ${resultadodiv}`);
